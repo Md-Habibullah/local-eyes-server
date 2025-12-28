7c6129ce21c3e3055622e10bc9cffd6826f473d6
Md Habibullah <mdhabibullah.work@gmail.com>
2025-12-28 15:40:06 +0600

    google client id secret added in env config

--- src/config/index.ts
+++ src/config/index.ts
@@ -27,6 +27,12 @@ export default {
         app_pass: process.env.APP_PASS,
     },
 
+    google: {
+        google_client_id: process.env.GOOGLE_CLIENT_ID,
+        google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        google_callback_url: process.env.GOOGLE_CALLBACK_URL,
+    },
+
     cloudinary: {
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
